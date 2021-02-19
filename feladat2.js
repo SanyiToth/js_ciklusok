9cdab69df873e302c447823b515b2020a7f43a45
Tóth Sándor <52709221+SanyiToth@users.noreply.github.com>
2021-02-19 14:37:53 +0100

    changes

--- feladat2.js
+++ feladat2.js
@@ -4,7 +4,7 @@ function primSzam(x) {
     if (x < 2) {    // 2-nél kisebb számot kiszűri, a legkisebb pímszám a 2.
         return false;
     }
-    for (let i = 2; i < x; i++) {      // a ciklus kiszűri a nem prímszámokat.
+    for (let i = 2; i <= (x/2); i++) {      // a ciklus kiszűri a nem prímszámokat.
         if (x % i == 0 && x > 2) {
             return false;
         }
